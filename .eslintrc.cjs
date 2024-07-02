1b682cfcd1b3c2fa9e3cb59bfe511448388e2ed6
alreagon <alpharajasa@gmail.com>
2024-07-02 13:56:13 +0700

    add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,22 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "airbnb"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react"],
   rules: {
     "react/jsx-uses-react": "2",
     "react/jsx-uses-vars": "error",
